2e1bf60e2037a5cb8742606c76d7fa9f6c2f38f7
yura-prots <yuraprots@gmail.com>
2022-11-12 19:19:57 +0200

    add interceptors

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
+import interceptors from "./interceptors";
 
 const instance = axios.create({
-  // baseUrl: process.env.VUE_APP_API_URL,
-  // params: {
-  //   apiKey: process.env.VUE_APP_API_KEY,
-  //   plot: "full",
-  // },
+  baseURL: process.env.VUE_APP_API_URL,
 });
+
+interceptors(instance);
 
 export default instance;
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,15 +1,39 @@
 import axios from "@/plugins/axios";
+import IDs from "@/store/mock/imdb_top";
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+  },
+
+  getters: {
+    slicedIDs:
+      ({ top250IDs }) =>
+      (from, to) =>
+        top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
+
   mutations: {},
+
   actions: {
-    async fetchMovies() {
-      const response = await axios.get(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=29c1b5e3"
-      );
+    async fetchMovies({ getters }) {
+      const { slicedIDs, currentPage, moviesPerPage } = getters;
+      const from = currentPage * moviesPerPage - moviesPerPage;
+      const to = currentPage * moviesPerPage;
+      const moviesToFetch = slicedIDs(from, to);
+      console.log(moviesToFetch);
+
+      const response = await axios.get("/", {
+        params: {
+          i: "tt3896198",
+        },
+      });
       console.log(response);
     },
   },
