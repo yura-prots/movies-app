1fae1cd09326e65b1642d5c2cf8bb102e9db7a11
yura-prots <yuraprots@gmail.com>
2022-11-13 22:36:30 +0200

    add MoviesList, MovieItem

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="movie-item">
+    <div class="movie-item-poster-wrap">
+      <div class="movie-item-poster" :style="posterBg">
+        <div
+          class="movie-item-info-wrap d-flex flex-column justify-content-between"
+        >
+          <div class="movie-item-info">
+            <h3 class="movie-title">{{ movieItem.Title }}</h3>
+            <span class="movie-year">{{ movieItem.Year }}</span>
+          </div>
+          <div class="movie-item-controls row row-cols-2">
+            <div class="col">
+              <button type="button" class="btn btn-outline-light btn-sm">
+                Edit
+              </button>
+            </div>
+            <div class="col">
+              <button type="button" class="btn btn-outline-light btn-sm">
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieItem",
+
+  props: {
+    movieItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    posterBg() {
+      return {
+        "background-image": `url(${this.movieItem.Poster})`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.movie-item {
+  position: relative;
+  cursor: pointer;
+  border-radius: 5px;
+  overflow: hidden;
+  transition: all 0.2s ease;
+  height: 400px;
+}
+
+.movie-item:hover {
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.7);
+  transform: scale(1.02);
+}
+
+.movie-item-poster {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+.movie-item-info-wrap {
+  padding: 20px 10px;
+  height: 100%;
+  opacity: 0;
+  transition: all 0.2s ease;
+}
+
+.movie-item:hover .movie-item-info-wrap {
+  opacity: 1;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+
+.movie-title {
+  font-size: 20px;
+  color: #fff;
+}
+
+.movie-year {
+  font-size: 14px;
+  color: #fff;
+}
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div class="container text-center">
+    <h3 class="list-title">IMDB Top 250</h3>
+    <div class="row g-4">
+      <template v-if="isExist">
+        <div class="col-3" v-for="(movie, key) in list" :key="key">
+          <movie-item :movieItem="movie"></movie-item>
+        </div>
+      </template>
+      <template v-else>
+        <div>Empty list</div>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
+import MovieItem from "./MovieItem.vue";
+
 export default {
   name: "MoviesList",
+
+  components: {
+    MovieItem,
+  },
 
   props: {
     list: {
@@ -12,7 +30,18 @@ export default {
       default: () => {},
     },
   },
+
+  computed: {
+    isExist() {
+      return Boolean(Object.keys(this.list).length);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.list-title {
+  font-size: 50px;
+  margin-bottom: 30px;
+}
+</style>
