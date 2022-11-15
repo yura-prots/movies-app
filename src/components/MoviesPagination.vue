432db41063f4247439497608aeb11ee2a3df6073
yura-prots <yuraprots@gmail.com>
2022-11-15 09:52:25 +0200

    add MoviesPagination

--- src/components/MoviesPagination.vue
+++ src/components/MoviesPagination.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>pagination</div>
+  <div>
+    <paginate
+      v-model="currentPage"
+      :page-count="total"
+      :page-range="perPage"
+      :prev-text="prevText"
+      :next-text="nextText"
+    />
+  </div>
+  {{ currentPage }}
 </template>
 
 <script>
+import Paginate from "vuejs-paginate-next";
+
 export default {
   name: "MoviesPagination",
+
+  components: {
+    Paginate,
+  },
 
   data() {
     return {
       currentPage: 1,
       total: 100,
-      perPage: 12,
+      perPage: 5,
+      prevText: "&laquo;",
+      nextText: "&raquo;",
     };
   },
 };
